0b614b40f803c85d2280f7c0e6297f16a5c1d4d0
AminaAshraf2002 <154620100+AminaAshraf2002@users.noreply.github.com>
2025-02-14 23:56:59 +0530

    Update enquiryRoutes.js

--- routes/enquiryRoutes.js
+++ routes/enquiryRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { 
-  createEnquiry, 
-  getAllEnquiries, 
-  updateEnquiryStatus, 
-  deleteEnquiry,
-  getEnquiryStats
+    createEnquiry, 
+    getAllEnquiries, 
+    updateEnquiryStatus, 
+    deleteEnquiry,
+    getEnquiryStats
 } = require('../controllers/enquiryController');
 const { protect } = require('../middleware/authMiddleware');
 
 // Public route for creating enquiry
-router.post('/blogs', createEnquiry);
+router.post('/', createEnquiry);
 
 // Protected admin routes
 router.get('/admin/enquiries', protect, getAllEnquiries);
-router.put('/:id/status', protect, updateEnquiryStatus);
-router.delete('/:id', protect, deleteEnquiry);
-router.get('/stats', protect, getEnquiryStats);
+router.put('/admin/enquiries/:id/status', protect, updateEnquiryStatus);
+router.delete('/admin/enquiries/:id', protect, deleteEnquiry);
+router.get('/admin/enquiries/stats', protect, getEnquiryStats);
 
 module.exports = router;
