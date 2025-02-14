116a3468f162a48efe1d279eca0a175b95b85194
AminaAshraf2002 <154620100+AminaAshraf2002@users.noreply.github.com>
2025-02-14 22:40:42 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,13 +16,13 @@ const loginValidation = [
 ];
 
 // Route to setup initial admin (one-time use)
-router.post('/admin/setup', setupAdmin);
+router.post('admin/setup', setupAdmin);
 
 // Admin Login Route
-router.post('/admin', loginValidation, loginAdmin);
+router.post('admin', loginValidation, loginAdmin);
 
 // Change Password Route (protected)
-router.put('/admin/change-password', 
+router.put('admin/change-password', 
   protect, 
   [
     body('currentPassword').not().isEmpty().withMessage('Current password is required'),
