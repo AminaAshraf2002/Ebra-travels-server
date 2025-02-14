6bc35884c0e16a7f6d9e6541cb1e90df0c1baf63
AminaAshraf2002 <154620100+AminaAshraf2002@users.noreply.github.com>
2025-02-14 23:51:34 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
-// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const { 
-  setupAdmin, 
-  loginAdmin, 
-  changePassword 
+const {
+  setupAdmin,
+  loginAdmin,
+  changePassword
 } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
@@ -19,16 +18,16 @@ const loginValidation = [
 router.post('/admin/setup', setupAdmin);
 
 // Admin Login Route
-router.post('/admin', loginValidation, loginAdmin);
+router.post('/admin/login', loginValidation, loginAdmin);
 
 // Change Password Route (protected)
-router.put('admin/change-password', 
-  protect, 
+router.put('/admin/change-password',
+  protect,
   [
     body('currentPassword').not().isEmpty().withMessage('Current password is required'),
     body('newPassword')
       .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-  ], 
+  ],
   changePassword
 );
 
